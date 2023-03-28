cc56fecb53b7b16c5905a05c21c80a0f79852294
Jieun Kim <jieunkim1379@gmail.com>
2023-03-28 15:02:13 -0400

    added post api

--- api/products.js
+++ api/products.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const {
   createProduct,
   getAllProducts,
   getProductById,
   updateProduct,
   destroyProduct,
-} = require('../db');
+  getProductById,
+} = require("../db");
 const productsRouter = express.Router();
 
 // GET /api/products
-productsRouter.get('/', async (req, res, next) => {
+productsRouter.get("/", async (req, res, next) => {
   try {
     const products = await getAllProducts();
 
@@ -23,8 +24,41 @@ productsRouter.get('/', async (req, res, next) => {
 });
 
 // POST /api/products **
-productsRouter.post('/')
+productsRouter.post("/", requireAdmin, async (req, res, next) => {
+  const {
+    name,
+    description,
+    price,
+    pic_url,
+    size,
+    inventory,
+    category_id,
+    color,
+    fragrance,
+  } = req.body;
 
-// DELETE /api/products/:product_id **
+  try {
+    const product = await createProduct({
+      name,
+      description,
+      price,
+      pic_url,
+      size,
+      inventory,
+      category_id,
+      color,
+      fragrance,
+    });
+
+    res.send({
+      success: true,
+      product,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 // PATCH /api/products/:product_id **
+
+// DELETE /api/products/:product_id **
