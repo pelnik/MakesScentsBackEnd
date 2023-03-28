b1e46ec8ba6c9e3edc28ce61f26e05e0b88793e7
Jieun Kim <jieunkim1379@gmail.com>
2023-03-28 14:40:53 -0400

    set up framework for products api calls

--- api/products.js
+++ api/products.js
@@ -1,9 +1,30 @@
-const express = require("express");
+const express = require('express');
 const {
   createProduct,
   getAllProducts,
   getProductById,
   updateProduct,
   destroyProduct,
-} = require("../db");
+} = require('../db');
 const productsRouter = express.Router();
+
+// GET /api/products
+productsRouter.get('/', async (req, res, next) => {
+  try {
+    const products = await getAllProducts();
+
+    res.send({
+      success: true,
+      products,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+// POST /api/products **
+productsRouter.post('/')
+
+// DELETE /api/products/:product_id **
+
+// PATCH /api/products/:product_id **
