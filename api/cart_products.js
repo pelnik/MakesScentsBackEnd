0adf1d3b591ee267a1b1894fb73851cb9d3dd9be
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-29 12:26:57 -0500

    update quantity ready

--- api/cart_products.js
+++ api/cart_products.js
@@ -2,7 +2,12 @@ const express = require('express');
 const cartProductsRouter = express.Router();
 
 const { requireUser } = require('./utils');
-const { getCartItems, getCartProduct, deleteCartItem } = require('../db');
+const {
+  getCartItems,
+  getCartProduct,
+  deleteCartItem,
+  updateCartItem,
+} = require('../db');
 
 cartProductsRouter.get('/', requireUser, async (req, res, next) => {
   try {
@@ -36,12 +41,8 @@ cartProductsRouter.delete(
       // Check if ID is in current user cart
       const cartProduct = await getCartProduct({ cart_product_id });
 
-      console.log('create product', cartProduct);
-
       if (cartProduct && cartProduct.user_id === user_id) {
         const deletedCartItem = await deleteCartItem({ cart_product_id });
-
-        console.log('deleted cart item', deletedCartItem);
 
         res.send({
           success: true,
@@ -62,5 +63,44 @@ cartProductsRouter.delete(
     }
   }
 );
+
+cartProductsRouter.patch('/:cart_product_id', async (req, res, next) => {
+  try {
+    const user_id = req.user.id;
+    const { quantity } = req.body;
+    let { cart_product_id } = req.params;
+    cart_product_id = Number(cart_product_id);
+
+    const cartProduct = await getCartProduct({ cart_product_id });
+
+    if (cartProduct === undefined) {
+      next({
+        name: 'InvalidCartProduct',
+        message: 'That cart product does not exist.',
+      });
+    } else if (cartProduct && cartProduct.user_id === user_id) {
+      const updatedItem = await updateCartItem({
+        cart_product_id,
+        quantity,
+      });
+
+      res.send({
+        success: true,
+        message: 'Quantity updated',
+        item: updatedItem,
+      });
+    } else {
+      next({
+        name: 'WrongUser',
+        message: "Cannot update someone else's cart item.",
+      });
+    }
+  } catch ({ name, message }) {
+    next({
+      name,
+      message,
+    });
+  }
+});
 
 module.exports = cartProductsRouter;
