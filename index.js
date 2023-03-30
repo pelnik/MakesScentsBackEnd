f395b304a076d2c605db5c42f55aefd4d013eb99
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-03-30 14:38:27 -0500

    fix cors error

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const client = require('./db/client');
 
 const PORT = 3000;
 
+const app = express();
+
 app.use(cors());
 app.use(morgan('dev'));
 
-const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
